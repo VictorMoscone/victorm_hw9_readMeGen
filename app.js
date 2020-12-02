8782db3e7eae6debcb9517dc44696b3f9b5d6a67
Victor Moscone <victormoscone@gmail.com>
2020-12-02 00:21:50 -0800

    Refactor & Feat: Changed the code to use an array of objects for the literals. I've added my typical Readme template and currently going through to add those objects.

--- app.js
+++ app.js
@@ -2,22 +2,84 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
-const writeFilePromt = util.promisify(fs.writeFile);
+const createFile = util.promisify(fs.writeFile);
 
-inquirer
-    .prompt([
-        { type: "input", name: "testInput", message: "enter a test" }
-    ])
-    .then(({ testInput, testInput3 }) => {
-        const str = `first test results: ${testInput}\nsecond test results ${testInput3}`;
-        
-        const rm = `
-        Test ${testInput} and ${testInput3}.
-        `;
+const userPrompts = () => {
+    return inquirer.prompt([
+        {
+            type: `input`,
+            name: `homeworkNumber`,
+            message: `What's the number of the assignment? Ex: 09`,
+        },
+    ]);
+};
 
-        return writeFilePromt("ReadMe.md", rm)
-    })
-    .then(() => console.log("complete"))
+const writeReadMe = (userInput) => 
+`
+# ${userInput.homeworkNumber} Victor Moscone's Weather Dashboard - HW6
 
-    .catch((err) => console.log(err));
+## Weather Report with a chance of Snail Rain.
 
+**A quick description**: Schönen tag! If you're looking for a regular weather report, then look no further. My good ol' weather reportin' app has just the means to roast your wurst. Whether you're looking to see today's forecast or the next *five days, we got it all!
+
+Just enter in the city name you want to search for and watch the magic happen. My app will dynamically update to whichever city you have entered and display for you a variety of realtime data. This includes:
+- City Name
+- Current Date
+- Current Weather Icon
+- Current  Temperature, Humidity, Wind Speed, and the UV Index (which will also be highlighted in a color, as a friendly reminder of the UV quality).
+- A 5-Day Forecast of your desired city.
+and of course,
+- A search history box of your most recent searches, up to 8 total!
+
+## Reflection
+
+Wow, there was a lot to do with this code. I worked non-stop for almost 3 days and constantly found myself at various hardpoints. I still have a lot to learn in regards to fetching APIs and how to best handle them. The asynchronous nature of fetches gave me a lot of trouble.
+If I were given more time to improve the app I would:
+1. Consolidate my functions better.
+2. Research and improve the error catching process.
+3. For Loops for the large ID Arrays.
+4. For Loops for the repetitive HTML elements.
+
+## Setup Instruction
+
+---
+1. Clone the repro to your desktop.
+2. Launch index.html.
+
+Alternatively:
+1. Visit https://victormoscone.github.io/victorm_hw6_weatherdash/
+---
+
+**Usage**
+
+* Primary Usage: Enter a city name and view the weather (current and next five days).
+* Seen below -
+
+![weather_mobile_1](https://user-images.githubusercontent.com/70674522/98459793-f9fbdd80-2152-11eb-971f-6616d4656b5a.PNG)
+![weather_web_1](https://user-images.githubusercontent.com/70674522/98459794-fa947400-2152-11eb-9cd0-34be180f6668.PNG)
+![weather_web_2](https://user-images.githubusercontent.com/70674522/98459795-fb2d0a80-2152-11eb-87a2-2878b71ff18f.PNG)
+
+**Credits** 
+
+* https://stackoverflow.com/questions/5898656/check-if-an-element-contains-a-class-in-javascript
+* https://stackoverflow.com/questions/9709758/dont-display-numbers-bullets-for-ordererd-or-unordered-list/9709788
+* https://stackoverflow.com/questions/19655189/javascript-click-event-listener-on-class
+* https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
+* https://19january2017snapshot.epa.gov/sunsafety/uv-index-scale-1_.html#:~:text=A%20UV%20Index%20reading%20of%203%20to%205%20means%20moderate,when%20the%20sun%20is%20strongest.&text=swimming%20or%20sweating.-,Watch%20out%20for%20bright%20surfaces%2C%20like%20sand%2C%20water%20and%20snow,reflect%20UV%20and%20increase%20exposure.
+
+- - -
+© 2020 Victor Moscone. All Rights Reserved.
+`;
+
+const initialize = async () => {
+    try {
+        const userInput = await userPrompts();
+        const readMe = writeReadMe(userInput);
+        await createFile(`ReadMe.md`, readMe);
+        console.log("Your file has been written.");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+initialize();
