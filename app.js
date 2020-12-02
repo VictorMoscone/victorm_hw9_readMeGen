20df454e18939309e555c0b50573d82f4d6dff25
Victor Moscone <victormoscone@gmail.com>
2020-12-01 23:35:13 -0800

    Feat: Added test logic to create a ReadMe file. Admittedly, I had to reference our class activities to get this going.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFilePromt = util.promisify(fs.writeFile);
+
+inquirer
+    .prompt([
+        { type: "input", name: "testInput", message: "enter a test" }
+    ])
+    .then(({ testInput, testInput3 }) => {
+        const str = `first test results: ${testInput}\nsecond test results ${testInput3}`;
+        
+        const rm = `
+        Test ${testInput} and ${testInput3}.
+        `;
+
+        return writeFilePromt("ReadMe.md", rm)
+    })
+    .then(() => console.log("complete"))
+
+    .catch((err) => console.log(err));
+
